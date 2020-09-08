dba455d60e7a3e7bfe7f59d6933a6eb9c3e61956
shutsuchi <wwww.www.wshu@gmail.com>
2020-09-08 21:31:27 +0900

    [create] set-up/home-controller+Top.vue

---
+++ app/javascript/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <Top/>
+  </div>
+</template>
+
+<script>
+import Top from './components/Top';
+
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  },
+  components: {
+    Top,
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
